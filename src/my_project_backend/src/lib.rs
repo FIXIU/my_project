2b3ec4b6c1109f1d67752501c0ce6f006b459a0d
FIXIU FIXIU <fixiuuuu@gmail.com>
2024-12-02 18:02:05 +0000

    Small fixes

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,8 +1,9 @@
-use::std::cell::RefCell;
+use std::cell::RefCell;
 
 thread_local! {
     static MSG: RefCell<String> = RefCell::new(String::new());
 }
+
 
 #[ic_cdk::query]
 fn get_msg() -> String{
@@ -10,11 +11,18 @@ fn get_msg() -> String{
 }
 
 #[ic_cdk::update]
-fn set_msg(new_msg: String){
-    MSG.with(|msg| *msg.borrow_mut() = new_msg);
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| *msg.borrow_mut() = new_msg)
 }
 
+// new_msg: String
+// msg.borrow_mut(): &mut String
+// &mut String != String ???
+// *(&mut String) == String
 
+
+
+//COUNTER.with(|count| *count.borrow_mut() = n);
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
